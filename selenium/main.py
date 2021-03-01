201a3c19ff83fed99ec0123c638b516086a8b853
Awfuldraw <71056297+Awfuldraw@users.noreply.github.com>
2021-03-01 15:56:02 +0900

    updated selenium

--- selenium/main.py
+++ selenium/main.py
@@ -4,16 +4,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # .\main.py
 
+KEYWORD = "buy domain"
+
+
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get("https://google.com")
 
 search_bar = browser.find_element_by_class_name("gLFyf")
-search_bar.send_keys("hello!")
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
 search_results = browser.find_elements_by_class_name("g")
-for result in search_results:
-    title = result.find_element_by_tag_name("h3")
-    if title:
-        print(title.text)
+for index, result in enumerate(search_results):
+    # class_name = result.get_attribute("class")
+    # g , g, g, g, g kno-kp ~~m, g, g
+    class_name = result.get_attribute("class")
+    if "kno-kp mnr-c g-blk" not in class_name:
+        result.screenshot(f"screenshots/{KEYWORD}_{index}.png")
+
+browser.quit()
